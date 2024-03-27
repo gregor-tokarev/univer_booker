8a4636df4df8956e187ee782d31d54f3f3e8e89f
gregor <e@tokarev.work>
2024-03-27 11:11:21 +0300

    Add heading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -16,17 +16,20 @@ export default function Home() {
   const placesResource = createAsync(() => loadPlaces());
 
   return (
-    <main class="mx-auto grid gap-4 p-4 text-gray-700 md:grid-cols-2">
-      <Show when={placesResource()}>
-        <For each={placesResource()}>
-          {(p) => (
-            <>
-              {" "}
-              <PlaceCard place={p} /> <PlaceCard place={p} />
-            </>
-          )}
-        </For>
-      </Show>
-    </main>
+    <div class="p-4 ">
+      <h1 class="mb-6 text-5xl text-slate-800">Доступные места</h1>
+      <div class="grid gap-4 text-gray-700 md:grid-cols-2">
+        <Show when={placesResource()}>
+          <For each={placesResource()}>
+            {(p) => (
+              <>
+                {" "}
+                <PlaceCard place={p} /> <PlaceCard place={p} />
+              </>
+            )}
+          </For>
+        </Show>
+      </div>
+    </div>
   );
 }

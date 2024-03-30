bf97ad54c6b2d6c8f59c4c45d06c6581d6781148
gregor <e@tokarev.work>
2024-03-30 22:43:01 +0300

    Make button `color` prop defaulted

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,4 +1,4 @@
-import { children, JSX } from "solid-js";
+import { children, JSX, mergeProps } from "solid-js";
 import { cn } from "~/lib/utils";
 
 interface ButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -6,18 +6,19 @@ interface ButtonProps extends JSX.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export default function Button(props: ButtonProps) {
-  const c = children(() => props.children);
+  const defaultedProps = mergeProps({ color: "accent" }, props);
+  const c = children(() => defaultedProps.children);
 
   return (
     <button
-      {...props}
+      {...defaultedProps}
       class={cn(
-        props.class,
+        defaultedProps.class,
         "w-full cursor-pointer rounded-md bg-sky-500 py-2 text-sky-50 transition-colors hover:bg-sky-600",
       )}
       classList={{
         "bg-slate-300 text-slate-800 hover:bg-slate-400":
-          props.color === "gray",
+          defaultedProps.color === "gray",
       }}
     >
       {c()}
